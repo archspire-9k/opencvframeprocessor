bef3c3589521d4b1ea82bdcb2a5238354f45ca75
archspire-9k <107560424+archspire-9k@users.noreply.github.com>
2023-05-26 12:14:59 -0400

    implemented frameProcessor plugin

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    'react-native-reanimated/plugin',
-    {
-      globals: ['__objectDetect'],
-    },
-  ],
+    ["react-native-reanimated/plugin", {
+      globals: [
+        "__objectDetect"
+      ]
+    }]
+  ]
 };
